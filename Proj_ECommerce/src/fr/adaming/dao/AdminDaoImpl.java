c674acd7a167cdb00c038cfecd9cd5c0774d5b7a
inti0343 <inti0343@DESKTOP-KI42TAP>
2018-03-29 16:12:37 +0200

    Fab : classes vides pour packages

---
+++ Proj_ECommerce/src/fr/adaming/dao/AdminDaoImpl.java
@@ -0,0 +1,11 @@
+package fr.adaming.dao;
+
+public class AdminDaoImpl implements IAdminDao {
+
+	@Override
+	public int ajouterProduit() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
