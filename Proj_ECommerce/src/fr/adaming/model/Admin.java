101d67813bbe9d5b445124e13fa04173fd4f8084
inti0343 <inti0343@DESKTOP-KI42TAP>
2018-03-29 13:25:09 +0200

    Fab : fichier admin sans UML

--- Proj_ECommerce/src/fr/adaming/model/Admin.java
+++ Proj_ECommerce/src/fr/adaming/model/Admin.java
@@ -1,5 +1,72 @@
 package fr.adaming.model;
 
-public class Admin {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "admins")
+public class Admin implements Serializable{
+	// Declaration des attributs
+	@Id
+	@Column(name = "id_ad")
+	private int id;
+	private String mail;
+	private String mdp;
+	
+	// Declaration des constructeurs
+	public Admin() {
+		super();
+	}
+
+	public Admin(String mail, String mdp) {
+		super();
+		this.mail = mail;
+		this.mdp = mdp;
+	}
+
+	public Admin(int id, String mail, String mdp) {
+		super();
+		this.id = id;
+		this.mail = mail;
+		this.mdp = mdp;
+	}
+	
+
+	// Declaration des getters et setters
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMail() {
+		return mail;
+	}
+
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+
+	public String getMdp() {
+		return mdp;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+
+	
+	// declaration des methodes
+	@Override
+	public String toString() {
+		return "Admin [id=" + id + ", mail=" + mail + ", mdp=" + mdp + "]";
+	}
+	
+	
 }
