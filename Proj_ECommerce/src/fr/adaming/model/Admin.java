d4025f1d388d271c50b10f3c2cc6060cf542890b
inti0343 <inti0343@DESKTOP-KI42TAP>
2018-03-29 14:47:37 +0200

    Fab : Admin avec association UML

--- Proj_ECommerce/src/fr/adaming/model/Admin.java
+++ Proj_ECommerce/src/fr/adaming/model/Admin.java
@@ -1,10 +1,12 @@
 package fr.adaming.model;
 
 import java.io.Serializable;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -16,6 +18,10 @@ public class Admin implements Serializable{
 	private int id;
 	private String mail;
 	private String mdp;
+	
+	// Tranformation association UML en Java
+	@OneToMany(mappedBy="admin")
+	private List<Produit> listeProduits;
 	
 	// Declaration des constructeurs
 	public Admin() {
